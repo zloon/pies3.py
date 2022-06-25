b1ed9e154a09e418497c573d38866ebdd7a19d4d
Viktor Lindgren <viktor.lindgren@gmail.com>
2022-06-25 14:49:43 +0200

    Added stubs & more pseudocode

--- pies3.py
+++ pies3.py
@@ -1,21 +1,72 @@
-#!/usr/bin/python3
+'''S3 Backup with pre-internet encryption'''
 
-## setup
-#db = dbconnection()
-#db.connect
+# TODO - add setup options
+# TODO - custom mappings of local directories to specific s3 buckets or paths
+# TODO - rate limit API calls, maybe with https://pypi.org/project/ratelimit/
 
-#for file in backupfolder:
-  #chksum = checksum(file)
-  #chksum_aws = dynamodb_select(file.name)
+'''Calculates the checksum of either a local file or an S3 object'''
+def calculate_checksum(file: str, algorithm: str = "SHA-256") -> str:
+  # If file is an S3 url
+    # Call S3 metadata to get checksum
 
-  ## improvement of below - batch uploading
-  #if chksum != chksum_aws:
-    #enc_file = encrypt(file,folder,key)
-    #s3 put enc_file
-    #dynamodb_insert(file.name,chksum)
+  # Else
+    # Check if file exists locally
+    # If it does, calculate & return checksum
+    # If not, throw error
 
-#def dynamodb_select(filename)
-  #return db.select(filename)
+  pass
 
-#def dynamodb_insert(filename,checksum)
-  #return db.insert(filename,checksum)
+'''Returns the full S3 path for a local file'''
+def s3_path(file_path: str, directory: str) -> str:
+  pass
+
+'''encrypts a file, placing it in the specified location'''
+def encrypt(source_file_path: str, encrypted_file_path: str) -> str:
+  pass # TODO - encrypt, return location of encrypted file
+
+'''uploads a file to s3 and sets the checksum'''
+def upload(file_path: str, checksum: str) -> str:
+  pass # TODO - upload to S3 and set checksum metadata
+
+uploadqueue = []
+tmp_directory = "" # TODO - Set tmp directory
+
+# Set up database connection
+# Either DynamoDB or local DB
+
+# Get backup folders for current host
+list_of_directories = [] # TODO - get this from database
+
+
+# Iterate over files in each backup folder
+for directory in list_of_directories:
+  checksum_local = ""
+
+  for file_path in directory:
+    # Calculate the file's checksum and the remote file's checksum
+    try:
+      checksum_local = calculate_checksum(file_path)
+    except FileNotFoundError as err:
+      pass # TODO - print error and skip
+
+    try:
+      checksum_s3 = calculate_checksum(s3_path(file_path, directory))
+    except FileNotFoundError as err: # This (likely?) means that there's no backup of the file yet. TODO - differentiate errors
+      checksum_s3 = ""
+      
+    # If the checksums doesn't match, add to queue for encryption & upload
+    if checksum_local != checksum_s3:
+      uploadqueue.append((file_path, checksum_local))
+
+# Iterate over the upload queue
+for file_path, checksum in uploadqueue:
+  # Encrypt the file
+  encrypted_file_path = encrypt(file_path, tmp_directory)
+
+  # Upload it to S3 and set metadata
+  try:
+    upload(encrypted_file_path, checksum)
+  except:
+    pass # TODO - error handling
+
+  # TODO - Remove temporary file when done
